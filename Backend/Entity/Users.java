f4c6246198d8f6ee6ff860edb0d595621ee01604
Alen Garibović <35613986+agaribovic@users.noreply.github.com>
2018-11-16 17:25:48 +0100

    Create Users.java

---
+++ Backend/Entity/Users.java
@@ -0,0 +1,181 @@
+package com.team1.demo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userID;
+
+    @NotNull
+    @Size(min=5, max=30)
+    private String username;
+
+    @NotNull
+    @Size(min=5)
+    private String password;
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String firstName;
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String lastName;
+
+    private double longitude;
+    private double latitude;
+
+    private String role;
+
+    private String email;
+    private String confirmToken;
+    private String reactivateToken;
+    private String passwordToken;
+
+
+
+    private Boolean enabled;
+
+
+    public Users(){}
+
+    public Users(String username, String password, String firstName, String lastName, double longitude, double latitude, String role) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.role = role;
+    }
+
+    public Users(Users user) {
+        this.role = user.role;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.username = user.username;
+        this.password = user.password;
+        this.longitude = user.longitude;
+        this.latitude = user.latitude;
+        this.passwordToken = user.passwordToken;
+        this.confirmToken = user.confirmToken;
+        this.reactivateToken = user.reactivateToken;
+        this.enabled = user.enabled;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getConfirmToken() {
+        return confirmToken;
+    }
+
+    public void setConfirmToken(String confirmToken) {
+        this.confirmToken = confirmToken;
+    }
+
+    public String getReactivateToken() {
+        return reactivateToken;
+    }
+
+    public void setReactivateToken(String reactivateToke) {
+        this.reactivateToken = reactivateToke;
+    }
+
+    public String getPasswordToken() {
+        return passwordToken;
+    }
+
+    public void setPasswordToken(String passwordToken) {
+        this.passwordToken = passwordToken;
+    }
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
