d536dc301cb21a723cd4e2bf3beae95c830273aa
Alen Garibović <35613986+agaribovic@users.noreply.github.com>
2018-11-16 17:23:07 +0100

    Create HotelController.java

---
+++ Backend/Controller/HotelController.java
@@ -0,0 +1,12 @@
+package com.team1.demo.Controller;
+
+import com.team1.demo.Services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HotelController {
+    private HotelService hotelService;
+
+
+}
