8c2bdd25d6076fcc5418a3aadb1fd60b017e1fdc
Alen Garibović <35613986+agaribovic@users.noreply.github.com>
2018-11-16 17:23:57 +0100

    Create TravelTrackerController.java

---
+++ Backend/Controller/TravelTrackerController.java
@@ -0,0 +1,9 @@
+package com.team1.demo.Controller;
+
+import org.apache.catalina.User;
+
+public class TravelTrackerController {
+    public void addUser(User user) {
+        // add new user
+    }
+}
