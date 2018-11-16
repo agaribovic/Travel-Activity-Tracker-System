24db090036bd600f135acb6e753cd14690c9ca69
Alen Garibović <35613986+agaribovic@users.noreply.github.com>
2018-11-16 17:25:07 +0100

    Team1Application.java

---
+++ Backend/Team1Application.java
@@ -0,0 +1,13 @@
+package com.team1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team1Application.class, args);
+        System.out.println("something");
+    }
+}
